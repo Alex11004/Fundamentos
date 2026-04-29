eefa14ea998f2101ddf11e1d7a42cc6ede180518
Alex11004 <castilloluisalejo12@gmail.com>
2026-04-28 20:55:37 -0500

    adelanto ejercicio tres

--- logica.js
+++ logica.js
@@ -15,7 +15,22 @@ console.log("Hola, " + nombre + ". Tienes " + edad + " años. ");
 • Calcula y muestra el área.
 */
 
+/*
 let base = prompt("Ingrese la base del rectángulo. ");
 let altura = prompt("Ingrese la altura del rectángulo. "); 
 let area = base * altura;
-console.log("El área del rectángulo es de " + area + " centímetros. ")
+console.log("El área del rectángulo es de " + area + " centímetros. ");
+*/
+
+/*
+3. Promedio de tres notas
+• Solicita tres calificaciones.
+• Calcula y muestra el promedio.
+*/
+
+let nota1 = prompt("Ingrese la primera nota: ");
+let nota2 = prompt("Ingrese la segunda nota: ");
+let nota3 = prompt("Ingrese la tercera nota: ");
+/*Falta revisar la fórmula porque el resultado no es correcto*/
+let promedio = (nota1 + nota2 + nota3) / 3;
+console.log("El promedio del estudiante es de " + promedio );
