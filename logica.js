ab430441ef419bc6345a60fe77bd0af51fa592db
Alex11004 <castilloluisalejo12@gmail.com>
2026-04-28 20:40:40 -0500

    ejercicio dos

--- logica.js
+++ logica.js
@@ -3,6 +3,19 @@ let nombre = prompt("Cuál es tu nombre?");
 console.log("Hola, " + nombre);
  */
 
+/*
 let nombre = prompt("Cuál es tu nombre?");
 let edad = prompt("Cuál es tu edad?")
 console.log("Hola, " + nombre + ". Tienes " + edad + " años. ");
+*/
+
+/*
+2. Área de un rectángulo
+• Solicita base y altura.
+• Calcula y muestra el área.
+*/
+
+let base = prompt("Ingrese la base del rectángulo. ");
+let altura = prompt("Ingrese la altura del rectángulo. "); 
+let area = base * altura;
+console.log("El área del rectángulo es de " + area + " centímetros. ")
