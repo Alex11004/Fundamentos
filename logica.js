5d154ae7c8998a2cfeb32643dddb2ca08541d4d3
Oscar Solano <mao090997gmail.com>
2026-04-28 20:20:14 -0500

    se crearon los archivos y se realizó el primer ejercicio

---
+++ logica.js
@@ -0,0 +1,8 @@
+/*
+let nombre = prompt("Cuál es tu nombre?");
+console.log("Hola, " + nombre);
+ */
+
+let nombre = prompt("Cuál es tu nombre?");
+let edad = prompt("Cuál es tu edad?")
+console.log("Hola, " + nombre + ". Tienes " + edad + " años. ");
